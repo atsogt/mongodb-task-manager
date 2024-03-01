b1551ead8e4f7f36cda71c92d74b7a94cb2bf852
Anand Tsogtbaatar <48449043+atsogt@users.noreply.github.com>
2024-03-01 16:57:04 -0500

    Update mongoose.js

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -5,7 +5,7 @@ const connectToDatabase = async () => {
     await mongoose
       .connect(
         // process.env.MONGODB_URL
-        "mongodb+srv://atsogt24:Cosset2011$@cluster0.c5f0oda.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+      
       )
       .then(() => {
         console.log("Connected to database");
